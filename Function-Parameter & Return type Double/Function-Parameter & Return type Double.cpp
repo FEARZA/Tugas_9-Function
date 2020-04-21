435ed960408264a33862d9094775b08e4888f9de
FEARZA KOSHI <muhammadkhosyi162@gmail.com>
2020-04-21 18:36:06 +0800

    Tugas_9 Function

---
+++ Function-Parameter & Return type Double/Function-Parameter & Return type Double.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+double pembagian (double a, double b){
+	return a / b;
+}
+
+int main (){
+	cout << pembagian (22.5, 7.9);
+}
---
+++ Function-Return only/Function-Return only.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int fungsi (){
+	int a = 10, b = 20;
+	return a * b;
+}
+
+int main (){
+	cout << fungsi ();
+}
